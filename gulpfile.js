6c068a80850e8410bdad47cc66de9e6d00f74f9b
Muhammad Syifa <emsifa@gmail.com>
2017-03-11 21:56:50 +0700

    Initialize Commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var stylus = require('gulp-stylus');
+var autoprefixer = require('autoprefixer-stylus');
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
+
+
+gulp.task('watch', function () {
+  return watch('src/stylus/**/*.styl', function() {
+  	gulp.src('src/stylus/skin-alague.styl')
+	.pipe(plumber({
+		errorHandler: notify.onError(("Error: <%= error.message %>"))
+	}))
+	.pipe(stylus({
+	  use: [autoprefixer('last 2 versions')]
+	}))
+	.pipe(gulp.dest('./dist/css'))
+	.pipe(notify("Updated: <%= file.relative %>!"))
+  })
+});
