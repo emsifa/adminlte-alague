a7067c2e64092a7bb568c7e6cffcad880ddf3c33
Muhammad Syifa <emsifa@gmail.com>
2017-03-12 00:44:07 +0700

    Fixing indentation

--- gulpfile.js
+++ gulpfile.js
@@ -5,17 +5,16 @@ var autoprefixer = require('autoprefixer-stylus');
 var notify = require('gulp-notify');
 var plumber = require('gulp-plumber');
 
-
 gulp.task('watch', function () {
   return watch('src/stylus/**/*.styl', function() {
-  	gulp.src('src/stylus/skin-alague.styl')
-	.pipe(plumber({
-		errorHandler: notify.onError(("Error: <%= error.message %>"))
-	}))
-	.pipe(stylus({
-	  use: [autoprefixer('last 2 versions')]
-	}))
-	.pipe(gulp.dest('./dist/css'))
-	.pipe(notify("Updated: <%= file.relative %>!"))
+    gulp.src('src/stylus/skin-alague.styl')
+    .pipe(plumber({
+      errorHandler: notify.onError(("Error: <%= error.message %>"))
+    }))
+    .pipe(stylus({
+      use: [autoprefixer('last 2 versions')]
+    }))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(notify("Updated: <%= file.relative %>!"))
   })
 });
